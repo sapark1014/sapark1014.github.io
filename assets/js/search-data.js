9f0760ca5d0dd1fe428fa8425fad8ea690659300
sapark1014 <117463202+sapark1014@users.noreply.github.com>
2026-03-17 14:44:12 +0000

    Deploying to gh-pages from @ sapark1014/sapark1014.github.io@c4e760c380b82d9df53ce9367b4717e8df298346 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,12 +423,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "News",},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
