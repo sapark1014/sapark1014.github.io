b24bc9e70a6e9458234d9c97147e59a3ae5a634f
sapark1014 <117463202+sapark1014@users.noreply.github.com>
2026-03-17 14:46:56 +0000

    Deploying to gh-pages from @ sapark1014/sapark1014.github.io@ba97c5c135d31d7f2caf564517cbe1ed154ad752 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,7 +423,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-imageinlife",
+          title: 'ImageInLife',
+          description: "EU H2020 Marie Skłodowska-Curie Innovative Training Networks (ITN)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/imageinlife/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
